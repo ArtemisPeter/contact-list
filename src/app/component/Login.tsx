c826defa12be585d11956591d6f3b9564f8d1881
Artemis <neilmarkluspo5@gmail.com>
2023-09-19 11:39:25 +0800

    using of ReactQuery

--- src/app/component/Login.tsx
+++ src/app/component/Login.tsx
@@ -1,42 +1,45 @@
-"use client"
-import React, { useState } from 'react'
+
+import React, { FormEvent, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 import style from '../page.module.css'
 import {BsPersonCircle} from 'react-icons/bs'
 import axios from 'axios'
+import { useMutation } from '@tanstack/react-query'
+import LoggedIn from './LoggedIn'
 
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [credentials, setCredentials] = useState([]);
-    const [success, setSuccess] = useState(false);
-    const [errmsg, seterrmsg] = useState("");
-   
-    const handleSubmit = async(event) =>{
-        event.preventDefault();
-        const email = event.target.elements[0].value;
-        const password = event.target.elements[1].value;
 
-        setEmail(email);
-        setPassword(password)
-        
-        const credentials = {
-            email,
-            password
+
+
+    const {isSuccess, isLoading, isError, mutate, reset} = useMutation ({
+        mutationFn: (data) => 
+            axios.post('https://hi-clist-be.vercel.app/api/user/login', data)
+            .then(response => response.data),
+            onSuccess: () => {
+                console.log('success');
+                
+            },
+            
+    })
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const form = event.target as HTMLFormElement;
+        const emailInput = form.querySelector('[name="username"]') as HTMLInputElement;
+        const passwordInput = form.querySelector('[name="password"]') as HTMLInputElement;
+
+        const LogInCredentials = {
+            email: emailInput.value,
+            password:passwordInput.value,
         }
-        
-        axios.post('https://hi-clist-be.vercel.app/api/user/login', credentials).then((response)=>{
-            setSuccess(true)
-        }).catch((error)=>{
-            seterrmsg((prevState)=> (prevState = `${error}`))
-        })
+        mutate(LogInCredentials);
     }
 
+  
   return (
     <>
-    {success ? (
-        <h1>Logged in</h1>
-    ) : ( <section>
+     {isSuccess ? <LoggedIn /> :
+     <section>
         <div className='container d-flex justify-content-center mt-5'>
             <div className='rounded-top'>
                 <div className={style.head}>
@@ -46,17 +49,16 @@ const Login = () => {
                     <BsPersonCircle size="50px" color="white" className = {style.icon}/>
                 </div>
                 <form className='p-4 form-group' style={{backgroundColor: "lightgray"}} onSubmit={handleSubmit}>
-                    <div className="text-center" style={{color: "red"}}>{errmsg}</div>
+                    <div className="text-center" style={{color: "red"}} onClick={reset}>{isError ? 'Failed': ''}</div>
                     <input type='text' name = "username" placeholder='User Name' className='form-control mt-3'/>
                     <input type="password" name = "password" placeholder='Password' className='form-control mt-3' />
                     <div className='d-grid gap'>
-                        <button type='submit' className='btn mt-4' style={{color: "grey", fontSize: "1.2rem"}}> Login</button>
+                        <button type='submit' className='btn mt-4' style={{color: "grey", fontSize: "1.2rem"}} disabled={isLoading}> {isLoading ? 'Logging in...' : 'Log In'}</button>
                     </div>
                 </form>
             </div>
         </div>
-        </section>)
-       }
+        </section>}
     </>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+"use client"
 import Login from './component/Login';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
 
 export default function Home() {
+  const queryClient = new QueryClient ();
   return (
     <>
+    <QueryClientProvider client={queryClient}>
       <Login />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+      
     </>
   )
 }
